465be6c40a8989539be9bf85b0019d695d3137c2
potados99 <potados99@gmail.com>
2019-02-05 07:47:52 +0900

    Transport

--- main.c
+++ main.c
@@ -15,8 +15,29 @@
 const char * hello_world = "+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.";
 
 int main(int argc, const char * argv[]) {
+    if (argc > 1) {
+        bf_run(argv[1], (int)strlen(argv[1]), 0);
+    }
+
+    char buf[512] = {0, };
+
+
+    while() {
+    rewind(stdin);
+    if (fgets(buffer, _BUFFER_SIZE - 1, stdin) == NULL) {
+        return NULL;
+    }
+    rewind(stdin);
+
+    if (strlen(buffer) == 1) {
+        return NULL;
+    }
+    }
+
     bf_run(hello_world, (int)strlen(hello_world), 0);
     puts("");
-    
+
+
+
     return 0;
 }
